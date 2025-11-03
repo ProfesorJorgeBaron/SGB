8a606de2e23ee0e95fef443f5a955199a63c82d1
jorgebaron <jorge.baron@iespoligonosur.org>
2025-11-03 10:22:01 +0100

    Correcciones

--- Redis/crearClavesValor.py
+++ Redis/crearClavesValor.py
@@ -1,6 +1,6 @@
 import redis
 
-conexionRedis = redis.ConnectionPool(host='localhost', port=6370, db=0,,decode_responses=True)
+conexionRedis = redis.ConnectionPool(host='localhost', port=6370, db=0,decode_responses=True)
 baseDatosRedis = redis.Redis(connection_pool=conexionRedis)
 
 baseDatosRedis.set('libro_1', 'Quijote')
